d991655b6c5434b35b76af7eb53d833f38ab137f
Théo LUDWIG <contact@theoludwig.fr>
2024-09-12 17:13:56 +0200

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# tp01_ludwig_theo
+
+[Théo LUDWIG](https://theoludwig.fr)
+
+## À propos
+
+Code réalisé dans le cadre de la formation [Ingénieur en Informatique et Systèmes d'Information (SI), CNAM](https://www.itii-alsace.fr/formations/informatique-et-systemes-dinformation-le-cnam/), pour le module Web.
+
+[SUJET](./SUJET.md)
---
+++ web/script.js
@@ -0,0 +1,108 @@
+/**
+ * @type {HTMLFormElement}
+ */
+const customerForm = document.getElementById('customerForm')
+
+/**
+ * @type {HTMLUListElement}
+ */
+const errorsList = document.getElementById('errors')
+
+/**
+ * @type {HTMLParagraphElement}
+ */
+const successMessage = document.getElementById('success')
+
+/**
+ * @type {HTMLUListElement}
+ */
+const customersList = document.getElementById('customersList')
+
+/**
+ * @type {HTMLDivElement}
+ */
+const customersSection = document.getElementById('customersSection')
+
+/**
+ * @typedef Customer
+ * @property {string} firstname
+ * @property {string} lastname
+ * @property {string} address
+ * @property {string} zipCode
+ * @property {string} city
+ * @property {string} phone
+ * @property {'man' | 'woman'} gender
+ * @property {string} login
+ * @property {string} email
+ * @property {string} password
+ * @property {string} passwordConfirmation
+ */
+
+/**
+ * @type {Customer[]}
+ */
+const customers = []
+
+customerForm.addEventListener('submit', (event) => {
+  event.preventDefault()
+  errorsList.innerHTML = ''
+
+  const formData = new FormData(customerForm)
+  const formDataObject = Object.fromEntries(formData)
+  const isSamePassword = formDataObject.password === formDataObject.passwordConfirmation
+  if (!isSamePassword) {
+    const li = document.createElement('li')
+    li.className = "error"
+    li.innerHTML = `
+      <strong>Erreur:</strong>
+      <span>Les mots de passe ne sont pas identiques.</span>
+    `
+    errorsList.appendChild(li)
+  }
+
+  const isValid = errorsList.children.length <= 0
+  if (isValid) {
+    errorsList.style.display = 'none'
+    successMessage.style.display = 'block'
+    customerForm.reset()
+    customers.push(formDataObject)
+    renderCustomers(customers)
+  } else {
+    errorsList.style.display = 'block'
+    successMessage.style.display = 'none'
+  }
+})
+
+/**
+ *
+ * @param {Customer[]} customers
+ */
+const renderCustomers = (customers) => {
+  if (customers.length > 0) {
+    customersSection.style.display = 'block'
+  } else {
+    customersSection.style.display = 'none'
+  }
+
+  customersList.innerHTML = ''
+  for (const customer of customers) {
+    const li = document.createElement('li')
+    li.innerHTML = `
+      <li>
+        <table class="table">
+          <thead>
+            <th>Prénom</th>
+            <th>Nom</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>${customer.firstname}</td>
+              <td>${customer.lastname}</td>
+            </tr>
+          </tbody>
+        </table>
+      </li>
+    `
+    customersList.appendChild(li)
+  }
+}
